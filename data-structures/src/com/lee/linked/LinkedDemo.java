eb6d4f843c6b6c8049dc577122fe80fc6d9bed70
guofulei <74183122+guofulei@users.noreply.github.com>
2022-11-04 15:57:37 +0800

    Add files via upload

--- data-structures/src/com/lee/linked/LinkedDemo.java
+++ data-structures/src/com/lee/linked/LinkedDemo.java
@@ -18,6 +18,7 @@ public class LinkedDemo {
             System.out.println("a->向链表添加元素");
             System.out.println("o->向链表添加元素并排序");
             System.out.println("s->展示链表");
+            System.out.println("r->移除链表中某个元素");
             System.out.print("请输入你的选择：");
             char c = scanner.next().charAt(0);
             switch (c){
@@ -34,12 +35,18 @@ public class LinkedDemo {
                 case 's':
                     linkedList.showLinked();
                     break;
+                case 'r':
+                    System.out.print("请输入number:");
+                    int n = scanner.nextInt();
+                    linkedList.removeNode(n);
+                    break;
                 default:
                     break;
             }
 
 
         }
+
         linkedList.showLinked();
     }
 }
@@ -74,10 +81,10 @@ class LinkedList{
             if(temp.nextNode==null){
                 break;
             }
-            if(temp.nextNode.no>=no){
+            if(temp.nextNode.no>no){
                 break;
             }
-            if (temp.no==no){
+            if (temp.nextNode.no==no){
                 throw new RuntimeException("该no已存在无法添加");
             }
             temp=temp.nextNode;
@@ -85,6 +92,22 @@ class LinkedList{
         Linked linkedNode=new Linked(no);
         linkedNode.nextNode=temp.nextNode;
         temp.nextNode=linkedNode;
+    }
+
+    public void removeNode(int no){
+         Linked temp=head;
+        while (temp.nextNode!=null){
+            if(temp.nextNode.no==no){
+                break;
+            }
+            temp=temp.nextNode;
+        }
+        if(temp.nextNode.nextNode!=null){
+            temp.nextNode=temp.nextNode.nextNode;
+        }else {
+            temp.nextNode=null;
+        }
+
     }
 
     public void showLinked(){
