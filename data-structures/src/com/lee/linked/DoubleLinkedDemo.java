7170870baad5aab86d19b9fa95d77ec37f586361
guofulei <74183122+guofulei@users.noreply.github.com>
2022-11-16 10:46:57 +0800

    Update DoubleLinkedDemo.java

--- data-structures/src/com/lee/linked/DoubleLinkedDemo.java
+++ data-structures/src/com/lee/linked/DoubleLinkedDemo.java
@@ -7,22 +7,36 @@ package com.lee.linked;
 public class DoubleLinkedDemo {
     public static void main(String[] args) {
         DoubleLinked doubleLinked = new DoubleLinked();
-        HeroNode2 linked1 = new HeroNode2(1, "linked1");
-        HeroNode2 linked2 = new HeroNode2(2, "linked2");
-        HeroNode2 linked3 = new HeroNode2(3, "linked3");
-        HeroNode2 linked4 = new HeroNode2(4, "linked4");
-        HeroNode2 linked5 = new HeroNode2(5, "linked5");
-        HeroNode2 linked6 = new HeroNode2(6, "linked6");
-        doubleLinked.add(linked1);
-        doubleLinked.add(linked2);
-        doubleLinked.add(linked3);
-        doubleLinked.add(linked4);
-        doubleLinked.add(linked5);
-        doubleLinked.add(linked6);
+//         HeroNode2 linked1 = new HeroNode2(1, "linked1");
+//         HeroNode2 linked2 = new HeroNode2(2, "linked2");
+//         HeroNode2 linked3 = new HeroNode2(3, "linked3");
+//         HeroNode2 linked4 = new HeroNode2(4, "linked4");
+//         HeroNode2 linked5 = new HeroNode2(5, "linked5");
+//         HeroNode2 linked6 = new HeroNode2(6, "linked6");
+//         doubleLinked.add(linked1);
+//         doubleLinked.add(linked2);
+//         doubleLinked.add(linked3);
+//         doubleLinked.add(linked4);
+//         doubleLinked.add(linked5);
+//         doubleLinked.add(linked6);
 
+//         System.out.println("链表为：");
+//         doubleLinked.show();
+//         doubleLinked.delete(3);
+        HeroNode2 linked1 = new HeroNode2(1, "linked1");
+        HeroNode2 linked2 = new HeroNode2(7, "linked7");
+        HeroNode2 linked3 = new HeroNode2(4, "linked4");
+        HeroNode2 linked4 = new HeroNode2(3, "linked3");
+        HeroNode2 linked5 = new HeroNode2(2, "linked2");
+        HeroNode2 linked6 = new HeroNode2(6, "linked6");
         System.out.println("链表为：");
+        doubleLinked.addOrderBy(linked1);
+        doubleLinked.addOrderBy(linked2);
+        doubleLinked.addOrderBy(linked3);
+        doubleLinked.addOrderBy(linked4);
+        doubleLinked.addOrderBy(linked5);
+        doubleLinked.addOrderBy(linked6);
         doubleLinked.show();
-        doubleLinked.delete(3);
         System.out.println("链表为：");
         doubleLinked.show();
     }
@@ -53,6 +67,40 @@ class DoubleLinked{
             }
             temp.next=node;
             node.pre=temp;
+        }
+    }
+    
+     public void addOrderBy(HeroNode2 node2) {
+        HeroNode2 temp = head;
+        boolean flag = false;
+        while (true) {
+            if (temp.next == null) {
+                break;
+            }
+            if (temp.next.no > node2.no) {
+                break;
+            }
+            if (temp.next.no == node2.no) {
+                flag = false;
+                break;
+            }
+            temp=temp.next;
+        }
+        if (!flag) {
+            //在尾部添加
+            if (temp.next == null) {
+                temp.next = node2;
+                node2.pre = temp;
+            }
+            //在temp temp.next 之间添加
+            else {
+                node2.pre = temp;
+                node2.next = temp.next;
+                temp.next = node2;
+                node2.next.pre = node2;
+            }
+        } else {
+            throw new RuntimeException("该序号已存在");
         }
     }
 
