7c8818cf02fc403994c1e1dd70b827916eee9e57
guofulei <74183122+guofulei@users.noreply.github.com>
2022-11-18 11:32:38 +0800

    Update CircleLinkedDemo.java

--- data-structures/src/com/lee/linked/CircleLinkedDemo.java
+++ data-structures/src/com/lee/linked/CircleLinkedDemo.java
@@ -7,8 +7,9 @@ package com.lee.linked;
 public class CircleLinkedDemo {
     public static void main(String[] args) {
         CircleLinked circleLinked = new CircleLinked();
-        circleLinked.creatCircleLinked(8);
+        circleLinked.creatCircleLinked(5);
         circleLinked.showCircleLinked();
+        circleLinked.outOfLoop(1,2);
     }
 }
 
@@ -42,6 +43,60 @@ class CircleLinked{
                 temp=temp.getNext();
             }
         }
+    }
+     /**
+     * 出圈
+     * @param startNo  从第startNo开始数
+     * @param countNumber   每次数countNumber个
+     */
+    public void outOfLoop(int startNo,int countNumber){
+        int circleLinkedSize = getCircleLinkedSize();
+        if(head==null && startNo<0 && startNo>circleLinkedSize && countNumber>circleLinkedSize ){
+            throw new RuntimeException("出圈数据错误");
+        }
+        // 辅助指针指向 head尾部
+        Node temp=head;
+        while (true){
+            if (temp.getNext()==head){
+                break;
+            }
+            temp=temp.getNext();
+        }
+        // 将head 和temp 同时移动startNo-1次
+        for(int i=1;i<=startNo-1;i++){
+            head=head.getNext();
+            temp=temp.getNext();
+        }
+       while (true){
+           if (temp==head){
+               break;
+           }
+           // 循环countNumber-1 次出圈
+           for (int i=1;i<=countNumber-1;i++){
+               head=head.getNext();
+               temp=temp.getNext();
+           }
+           System.out.println("出圈的NO为:"+head.toString());
+           head=head.getNext();
+           temp.setNext(head);
+       }
+        System.out.println("最后出圈的NO为:"+head.toString());
+    }
+
+    public int getCircleLinkedSize(){
+        if (head==null){
+            throw new RuntimeException("链表为空");
+        }
+        Node temp=head;
+        int size=0;
+        while (temp!=null){
+            size++;
+            if (temp.getNext()==head){
+                break;
+            }
+            temp=temp.getNext();
+        }
+        return size;
     }
 }
 
