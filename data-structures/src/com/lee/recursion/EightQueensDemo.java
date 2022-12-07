79aadd1ddea478a77f8908ce0384029e7bb5154c
guofulei <74183122+guofulei@users.noreply.github.com>
2022-12-07 11:13:55 +0800

    Update EightQueensDemo.java

--- data-structures/src/com/lee/recursion/EightQueensDemo.java
+++ data-structures/src/com/lee/recursion/EightQueensDemo.java
@@ -14,7 +14,8 @@ public class EightQueensDemo {
     public static void main(String[] args) {
         check(0);
     }
-
+    
+    //放入第n个皇后
     public static void check(int n) {
         if (n == max) {
             showQueens();
@@ -22,15 +23,27 @@ public class EightQueensDemo {
         }
         for (int i = 0; i < queens.length; i++) {
             queens[n] = i;
+            //第n个皇后和之前的皇后位置是否有冲突
             if (isConflict(n)) {
+                //如果没有冲突 放入第n+1给皇后 （递归）
                 check(n + 1);
             }
         }
 
     }
-
+    
+    //第n个皇后位置与之前的皇后位置有冲突
     public static boolean isConflict(int n) {
         for (int i = 0; i < n; i++) {
+            /**
+            *  如果n等于2 会遍历两次 会拿 n皇后和n之前的皇后比较是否在一列
+            *  queens[0]==queens[2]
+            *  queens[1]==queens[2] 
+            * 
+            * 如果 Math.abs(n-i)==Math.abs(queens[n]-queens[i])
+            * 说明皇后i和皇后n在同一斜线
+            * i每次都在换行 不用担心在同一行中
+            */
             if (queens[i] == queens[n] || Math.abs(n - i) == Math.abs(queens[n] - queens[i])) {
                 return false;
             }
